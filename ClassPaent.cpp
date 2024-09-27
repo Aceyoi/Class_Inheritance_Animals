1300180a1af5f55d946f3e5e45fb1d3d00304cab
Aceyoi <146310875+Aceyoi@users.noreply.github.com>
2024-09-27 10:28:08 +0900

    Добавьте файлы проекта.

---
+++ ClassPaent.cpp
@@ -0,0 +1,45 @@
+﻿// Код создан Сапожниковым Ю.С.
+// Создание классов наследования
+// Используемые библиотеки
+
+#include <iostream>
+#include <string>
+#include <cassert>
+#include "Classchild.h"
+
+
+using namespace std;
+
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+
+    //Cat cattest{ "1", 5, "1", "Белый" };
+    //Dog dogtest{ "2", 12, "2", "Мячик" };
+
+    //assert(cattest.age == 5);
+    //assert(cattest.name == "1");
+    //assert(cattest.breed == "1");
+
+    //assert(dogtest.age == 12);
+    //assert(dogtest.name == "2");
+    //assert(dogtest.breed == "2");
+
+
+    Animal* mira = new Cat("Мира", 1, "Сиамская", "Белый");
+    Animal* psina = new Dog("Псина", 12, "Овчарка", "Мячик");
+
+
+    //  Полиморфизм для вывода информации о животных
+    printanimal(mira);   // Вызовет print() для кошки
+    cout << endl;
+    printanimal(psina);  // Вызовет print() для собаки
+
+
+    // Освобождаем память, выделенную для объектов
+    delete mira;   
+    delete psina;  
+    return 0;
+}
+
---
+++ Classchild.h
@@ -0,0 +1,73 @@
+// Код создан Сапожниковым Ю.С.
+// Создание классов наследования
+// Используемые библиотеки
+
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+// Абстрактный класс Animal
+class Animal {
+public:
+	// Конструктор
+	Animal(string n, size_t a, string b) : name(n), age(a), breed(b) {
+		if (a < 0 || a > 20) { throw runtime_error("Ошибка: недопустимый возраст"); }
+	}
+
+	// Виртуальный метод делает класс абстрактным
+	virtual void print() const = 0;
+
+	// Виртуальный деструктор
+	virtual ~Animal() {}
+
+protected:
+	string name;    // Кличка животного
+	size_t age;     // Возраст животного
+	string breed;   // Порода животного
+};
+
+// Класс Cat наследуемый от Animal
+class Cat: public Animal {
+public:
+	// Конструктор
+	Cat(string name, size_t age, string breed, string furColor)
+		: Animal{ name, age, breed }, furColor(furColor)
+	{}
+	//Методы
+	void print() const override {
+		cout << "Это кошка!" << endl;
+		cout << "Кличка: " << name << endl
+			<< "Возраст: " << age << endl
+			<< "Порода: " << breed << endl
+			<< "Цвет шерсти: " << furColor << endl;
+	}
+
+private:
+	string furColor;
+};
+
+// Класс Dog наследуемый от Animal
+class Dog: public Animal {
+public:
+	// Конструктор
+	Dog(string name, size_t age, string breed, string favoriteToy)
+		: Animal{ name, age, breed }, favoriteToy(favoriteToy)
+	{}
+	//Методы
+	void print() const override{
+		cout << "Это собака!" << endl;
+		cout << "Кличка: " << name << endl
+			<< "Возраст: " << age << endl
+			<< "Порода: " << breed << endl
+			<< "Любимая игрушка: " << favoriteToy << endl;
+	}
+
+private:
+	string favoriteToy;
+};
+
+void printanimal(const Animal* animal) {
+	animal->print();  // Здесь происходит полиморфный вызов метода print()
+}
